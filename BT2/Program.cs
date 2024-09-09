85dcfdb3ae0edce19a03c59ea08a6d203c60bddb
hoatx611 <hoatx611@gmail.com>
2024-09-09 11:36:40 +0700

    Xét  học bổng cho sinh viên

--- BT2/Program.cs
+++ BT2/Program.cs
@@ -8,7 +8,45 @@ using BT2;
 //sv1.InThongTin();
 //Console.ReadKey();
 
+List<SinhVien> ds_sinhvien = new List<SinhVien>();
 
 var sv1 = SinhVien.NhapThongTin();
+ds_sinhvien.Add(sv1);
 Console.ReadKey();
-sv1.InThongTin();
+
+var sv2 = SinhVien.NhapThongTin();
+ds_sinhvien.Add(sv2);
+Console.ReadKey();
+
+var sv3 = SinhVien.NhapThongTin();
+ds_sinhvien.Add(sv3);
+Console.ReadKey();
+
+var sv4 = SinhVien.NhapThongTin();
+ds_sinhvien.Add(sv4);
+Console.ReadKey();
+
+var sv5 = SinhVien.NhapThongTin();
+ds_sinhvien.Add(sv5);
+Console.ReadKey();
+
+List<SinhVien> ds_hocbong = new List<SinhVien>();
+
+ds_sinhvien.ForEach(sv_tmp =>
+{
+    if(sv_tmp.Diem_tichluy >= 2.5 && sv_tmp.Diem_renluyen >= 8.0)
+    {
+        ds_hocbong.Add(sv_tmp);
+    }
+});
+
+ds_hocbong.OrderByDescending(s => s.Diem_tichluy).ToList();
+
+foreach(var d in ds_hocbong)
+{
+    d.InThongTin();
+}
+
+Console.ReadKey();
+
+
